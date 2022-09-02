ee92757c80e928f9022776efad40bf54f2f97ff2
puspakirana <puspakiranastv@gmail.com>
2022-09-02 18:37:19 +0700

    Lesson 7 - NodeJS Core Module

---
+++ 7-node-core-module/app.js
@@ -0,0 +1,51 @@
+//Core module File System
+const fs = require('fs');
+
+
+//menuliskan string ke file (synchronous)
+// try{
+//     fs.writeFileSync('data/test.txt', 'Hello world secara synchronous!');
+// }
+// catch(e){
+//     console.log(e);
+// }
+
+//menuliskan string ke file asynchronous
+// fs.writeFile('data/test.txt', 'Hello World secara asynchronous', (err) => {
+//     console.log(err);
+// });
+
+//membaca isi file synchronous
+// const data = fs.readFileSync('data/test.txt', 'utf-8');
+// console.log(data);
+
+//membaca isi file asynchronous
+// fs.readFile('data/test.txt', 'utf-8', (e, data) => {
+//     if(e) throw e;
+//     console.log(data);
+// });
+
+
+//Readline
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Masukkan nama anda: ', (nama) => {
+    rl.question('Masukkan no HP anda: ', (noHP) => {
+
+        const contact = {nama, noHP};
+        const file = fs.readFileSync('data/contacts.json', 'utf-8');
+        const contacts = JSON.parse(file);
+        contacts.push(contact);
+
+        fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+
+        console.log(`Terima kasih ${nama}, sudah menginput ${noHP}`);
+        
+        rl.close();
+    });
+});
