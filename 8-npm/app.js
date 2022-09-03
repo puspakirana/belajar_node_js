5415950ec84d7abb89b1d4a5c4d09f672a4cf7f1
puspakirana <puspakiranastv@gmail.com>
2022-09-03 13:28:23 +0700

    Lesson 9 - Package NPM (Validator, Chalk, Nodemon)

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,16 @@
-console.log("Hello world!");
+const validator = require('validator');
+const chalk = require('chalk');
+
+//VALIDATOR
+// console.log(validator.isEmail('puspakirana@gmail.com'));
+// console.log(validator.isMobilePhone('0812345678', 'id-ID'));
+// console.log(validator.isNumeric('0712345678a'));
+
+//CHALK
+console.log(chalk.bgBlue.italic.black('Hello'));
+const pesan = 'Hello world';
+console.log(chalk.bgRed.black(pesan));
+const nama = "Jane"
+const pesan2 = chalk `Lorem, ipsum dolor {bgRed.italic sit amet} consectetur {bgGreen.strikethrough adipisicing} elit. Nama saya : ${nama}`
+console.log(pesan2);
+
